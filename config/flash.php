b0256e22e357b158ce46284b5192c7838d2a5ae4
Edward Mann <the@eddmann.com>
2012-07-16 18:34:40 +0100

    initial commit
    The initial commit to the CodeIgniter Flash Message Library.
    Provides Spark manifest for easy installation into a project.

---
+++ config/flash.php
@@ -0,0 +1,68 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------
+|  Flash Session Name
+| -------------------------------------------------------------------
+| 
+| The name of the flash session to store all messages into.
+|
+*/
+
+$config['flash']['session_name'] = 'flash';
+
+
+/*
+| -------------------------------------------------------------------
+|  Default Flash Styling 
+| -------------------------------------------------------------------
+| 
+| The default styling (before/after) surrounding each flash message displayed.
+|
+*/
+
+$config['flash']['default_style'] = array('<div class="alert">', '</div>');
+
+
+/*
+| -------------------------------------------------------------------
+|  Custom Flash Styles 
+| -------------------------------------------------------------------
+| 
+| Custom styling used for specific flash message types.
+|
+*/
+
+$config['flash']['styles'] = array(
+  'success' => array('<div class="alert alert-success">', '</div>'),
+  'error'   => array('<div class="alert alert-error">', '</div>'),
+  'form'    => array('<div class="alert alert-error">', '</div>')
+);
+
+
+/*
+| -------------------------------------------------------------------
+|  Split Messages by Default
+| -------------------------------------------------------------------
+| 
+| Option to split flash messages into their own seperate alerts by default.
+|
+*/
+
+$config['flash']['split_default'] = FALSE;
+
+
+/*
+| -------------------------------------------------------------------
+|  Merge Form and Error Messages
+| -------------------------------------------------------------------
+| 
+| Option to merge both form validation errors and custom error messages.
+|
+*/
+
+$config['flash']['merge_form_errors'] = TRUE;
+
+
+/* End of file flash.php */
+/* Location: ./sparks/flash/1.0.0/config/flash.php */
---
+++ libraries/Flash.php
@@ -0,0 +1,278 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * CodeIgniter Flash Message Library
+ * 
+ * @author     Edward Mann <the@eddmann.com>
+ * @link       http://eddmann.com/
+ * @version    1.0.0
+ * @license    MIT License Copyright (c) 2012 Edward Mann
+ */
+class Flash {
+
+  /**
+   * The current CodeIgniter instance
+   *
+   * @var       object
+   * @access    private
+   */
+  private $_CI;
+
+  /**
+   * The messages retrieved from the session
+   *
+   * @var       array
+   * @access    private
+   */
+  private $_session_messages = array();
+
+  /**
+   * The messages stored for displaying on this request
+   *
+   * @var       array
+   * @access    private
+   */
+  private $_messages = array();
+
+  /**
+   * The permitted config options for alteration
+   *
+   * @var       array
+   * @access    private
+   */
+  private $_config_whitelist = array(
+    'session_name', 'default_style', 'styles', 'split_default', 'merge_form_errors'
+  );
+
+  /**
+   * The session name used for flashdata
+   *
+   * @var       string
+   * @access    public
+   */
+  public $session_name = 'flash';
+
+  /**
+   * The default styling used if none specified
+   *
+   * @var       array
+   * @access    public
+   */
+  public $default_style = array('<div>', '</div>');
+
+  /**
+   * The different styles used for specific message types
+   *
+   * @var       array
+   * @access    public
+   */
+  public $styles = array();
+
+  /**
+   * Split the displayed messages by default
+   *
+   * @var       bool
+   * @access    public
+   */
+  public $split_default = FALSE;
+
+  /**
+   * Merge form validation errors with error messages
+   *
+   * @var       bool
+   * @access    public
+   */
+  public $merge_form_errors = TRUE;
+
+  /**
+   * Constructer
+   *
+   * Retrieves the CI instance in use and then attempts to
+   * override the config options based on the config file, then
+   * passed in options.
+   *
+   * @param     array     $config
+   * @access    public
+   */
+  public function __construct(array $config = array())
+  {
+    $this->_CI =& get_instance();
+
+    $this->_CI->load->library('session');
+
+    if (is_array(config_item('flash')))
+      $config = array_merge(config_item('flash'), $config);
+
+    if ( ! empty($config))
+      $this->_initialize($config);
+  }
+
+  /**
+   * Initalize Class
+   *
+   * Overrides the whitelisted default config options with the
+   * ones passed in as a param.
+   *
+   * @param     array      $config
+   * @access    private
+   */
+  private function _initialize(array $config = array())
+  {
+    foreach ($config as $key => $value) {
+      if (in_array($key, $this->_config_whitelist)) {
+        $this->{$key} = $value;
+      }
+    }
+  }
+
+  /**
+   * Add Message
+   *
+   * Adds a message to the specified types array - you have
+   * the option to display the message on this request, else it
+   * will be stored in flashdata for the next one.
+   *
+   * @param     mixed      $msg     The message to be added
+   * @param     string     $type    The type of message being added
+   * @param     bool       $now     Display the message on this request or the next
+   * @access    private
+   * @return    object     $this
+   */
+  private function _add_message($msg, $type = 'default', $now = FALSE)
+  {
+    // all messages must scalar types (int, float, string or boolean)
+    // and the type must be a string, if either invalid an exception is raised
+    if ( ! is_scalar($msg) || ! is_string($type))
+      throw new Exception('Invalid message type/value entered.');
+
+    if ($now === FALSE) {
+      $this->_session_messages[$type][] = $msg;
+      $this->_CI->session->set_flashdata($this->session_name, $this->_session_messages);
+    }
+    else {
+      $this->_messages[$type][] = $msg;
+    }
+
+    return $this;
+  }
+
+  /**
+   * Display Messages
+   * 
+   * Returns the HTML to display the specified type in either
+   * split or joined message format. If no type specified all
+   * types are returned. If 'form' is passed in as the type the form
+   * validation class is used to retrieve the errors.
+   *
+   * @param     string    $type     The message type to display
+   * @param     bool      $split    Display messages split or joined
+   * @access    public
+   * @return    string    the message HTML
+   */
+  public function display($type = '', $split = NULL)
+  {
+    $session_messages = $this->_CI->session->flashdata($this->session_name);
+    $messages         = $this->_messages;
+
+    // attempt to display form errors if no type or form/error types passed in
+    if ($type === '' || $type === 'form' || ($this->merge_form_errors && $type === 'error')) {
+      $this->_CI->load->library('form_validation');
+
+      // check that validation errors function exists and return errors in an array
+      // if not, set to an empty array
+      $form_errors = (function_exists('validation_errors')) ?
+        explode('|', validation_errors('', '|')) : array();
+
+      foreach ($form_errors as $error) {
+        // add form message to error messages if merge specified and type valid
+        if ($this->merge_form_errors && ($type === '' || $type === 'error')) {
+          $messages['error'][] = $error;
+        }
+        // if not add error to forms own messages
+        else {
+          $messages['form'][] = $error;
+        }
+      }
+    }
+
+    // set split option to default if no option passed in
+    if ($split === NULL)
+      $split = $this->split_default;
+
+    // set the messages to a specific type if option present, else set to empty array
+    if ($type !== '') {
+      $session_messages = (isset($session_messages[$type])) ? 
+        array($type => $session_messages[$type]) : array();
+      $messages = (isset($messages[$type])) ? 
+        array($type => $messages[$type]) : array();
+    }
+
+    // merge session messages into current requests array if not empty
+    if (is_array($session_messages) && ! empty($session_messages))
+      $messages = array_merge_recursive($session_messages, $messages);
+
+    $output = '';
+
+    if ( ! empty($messages)) {
+      // loop through all message types if array not empty
+      foreach ($messages as $type => $msgs) {
+        // set the selected style based on type or use default
+        $selected_style = (isset($this->styles[$type])) ?
+          $this->styles[$type] : $this->default_style;
+
+        // output beginning style if split is false
+        if ( ! $split)
+          $output .= $selected_style[0] . '<ul>';
+
+        foreach ($msgs as $msg) {
+          // output full message style with message if split is true
+          if ($split) {
+            $output .= $selected_style[0] . $msg . $selected_style[1];
+          }
+          // output as a list element if not
+          else {
+            $output .= '<li>' . $msg . '</li>';
+          }
+        }
+        
+        // output ending style if split is false
+        if ( ! $split)
+          $output .= '</ul>' . $selected_style[1];
+      }
+    }
+
+    return $output;
+  }
+
+  /**
+   * Call Magic Method
+   *
+   * Used to allow user to call the class with a message type as the function name.
+   * When called it internally invokes the private add message function.
+   *
+   * @param     string    $name         The message type name
+   * @param     array     $arguments    The arguments passed into the method
+   * @access    public
+   * @return    object    $this
+   */
+  public function __call($name, $arguments)
+  {
+    if ( ! empty($arguments)) {
+      // set the message and display status
+      $msg = $arguments[0];
+      $now = (isset($arguments[1]) && $arguments[1] === TRUE);
+
+      // call the private add message method with provided arguments
+      return $this->_add_message($msg, $name, $now);
+    }
+    // throw a bad method exception if no arguments passed
+    else {
+      throw new BadMethodCallException();
+    }
+  }
+
+}
+
+
+/* End of file Flash.php */
+/* Location: ./sparks/flash/1.0.0/libraries/Flash.php */
