98852151a7241ca02f6c0c54e1b63150d1edd5c7
Edward Mann <the@eddmann.com>
2012-08-24 19:12:51 +0100

    Updated to set now message with _now instead of TRUE.
    Changed the way in which you set a message to be displayed this page cycle.
    Instead of passing a TRUE value as a message param - you now append '_now' to the function call,
    which is removed from name before being used as message type.

--- libraries/flash.php
+++ libraries/flash.php
@@ -274,12 +274,12 @@ class Flash {
 	public function __call($name, $arguments)
 	{
 		if ( ! empty($arguments)) {
-			// set the message and display status
-			$message     = $arguments[0];
-			$display_now = (isset($arguments[1]) AND $arguments[1] === TRUE);
+			// set display status based on function call name and set message
+			$name    = preg_replace('/_now$/', '', $name, 1, $display_now);
+			$message = $arguments[0];
 
 			// call the private add message method with provided arguments
-			return $this->_add_message($message, $name, $display_now);
+			return $this->_add_message($message, $name, (bool)$display_now);
 		}
 		// throw a bad method exception if no arguments passed
 		else {
